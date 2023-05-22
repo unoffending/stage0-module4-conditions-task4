0231d3feab088e7c7af5062fbbe8e786f75e276b
unoffending <109032978+unoffending@users.noreply.github.com>
2023-05-22 19:45:32 +0600

    Update TernaryGreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        int max = first > second ? first : second;
-        System.out.println(max);
+        if(first == second) {
+            System.out.println(first);
+        } else {
+            int max = first > second ? first : second;
+            System.out.println(max);
+        }
     }
 }
